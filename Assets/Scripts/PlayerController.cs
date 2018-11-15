fc63e31e2245fe4685432aaf92e8b7cc6efb3b36
Juraj Kubričan <kubrican.juraj@gmail.com>
2018-11-15 13:32:44 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    public float speed = 24;
+    public bool primary = true;
+    private int count;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (primary) {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            rb.AddForce(movement * speed);
+        }
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+
+    }
+}

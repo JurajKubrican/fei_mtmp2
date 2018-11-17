46798bad16e5014916c41a53ae2c7e7686187213
Juraj Kubričan <kubrican.juraj@gmail.com>
2018-11-17 12:00:57 +0100

    progress

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,24 +11,35 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 24;
     public bool primary = true;
-    private int count;
     private Rigidbody rb;
+    private GameObject player;
+    private Vector3 initialOffset;
 
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        count = 0;
+        player = GameObject.FindGameObjectWithTag("Player");
+        initialOffset = rb.transform.position - player.transform.position;
     }
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (primary) {
+        if (this.tag == "Player")
+        {
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
             Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
             rb.AddForce(movement * speed);
-        }
+        }else{
+            Vector3 position = player.transform.position;
+            Debug.Log(position);
+            position.x = - position.x;
+            position.z = - position.z;
+            Debug.Log(position);
+            Quaternion rotation = player.transform.rotation;
+            rb.transform.SetPositionAndRotation(position - initialOffset, rotation);
+        }   
         
     }
 
